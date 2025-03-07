e122940fd5b934d6486be29dc9e4e93e6559f9ac
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2025-03-06 21:36:29 -0800

    init commit
    finished and implemented extra credit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
+import { useEffect, useState } from "react";
+import CardDetail from "./components/CardDetail";
+import Cards from "./components/Cards";
 import "./App.css";
 
 function App() {
+  const [characters, setCaracters] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchCharacters(page);
+  }, [page]);
+
+  const fetchCharacters = async (page) => {
+    let res = await fetch("https://swapi.dev/api/people/?page=${page}");
+    let data = await res.json();
+    setCaracters(data.results);
+  };
+
+  const handleNext = () => {
+    setPage((prevPage) => prevPage + 1);
+    setSelectedCharacter(null);
+  };
+
+  const handleBack = () => {
+    if (page > 1) {
+      setPage((prevPage) => prevPage - 1);
+      setSelectedCharacter(null);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Star Wars Characters</h1>
       <div className="main-container ">
-        <div clssName>
-          <div>
-            <button>Back</button>
-            <button>Next</button>
-          </div>
+        <div>
+          <Cards
+            characters={characters}
+            onCharacterClick={fetchCharacterDetails}
+          />
         </div>
+        <button onClick={handleBack} disabled={page === 1}>
+          BACK
+        </button>
+        <button onClick={handleNext}>NEXT</button>
       </div>
     </div>
   );
